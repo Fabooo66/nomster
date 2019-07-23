558bee8ac469faa2da2d0c5c80c77a120373057f
Stephen Cowen <sclc_mail@yahoo.com>
2019-07-23 02:12:54 +0000

    geocoder1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
   validates :name, presence: true, length: { maximum:40, minimum: 3 } 
   validates :description, presence: true, length: { maximum:400, minimum: 3 } 
   validates :address, presence: true 
+
 end
