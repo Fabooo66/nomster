bb9354dbae9ed95e79d3082cb20d2f0ec4b19324
Stephen Cowen <sclc_mail@yahoo.com>
2019-08-01 05:07:38 +0000

    dashboard5

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -15,7 +15,7 @@ class Comment < ApplicationRecord
     RATINGS.invert[self.rating]
   end
 
-    def send_comment_email
+  def send_comment_email
       NotificationMailer.comment_added(self).deliver_now
   end
 
